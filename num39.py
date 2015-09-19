2280e4fb5446a95b7e17bc790146f0637c517cc0
Abhiram <abhi.darkness@gmail.com>
2015-09-19 21:37:25 +0530

    Added num 39

---
+++ num39.py
@@ -0,0 +1,19 @@
+import itertools
+
+li=range(1,1001)
+cnt=0
+for i in itertools.combinations(li,3):
+    cnt=cnt+1
+li2=[0]*cnt
+li3=[0]*1001
+for i in itertools.combinations(li,3):
+    t=sum(i)
+    if (((i[0]**2)+(i[1]**2))==(i[2]**2)):
+        li2[t]=li2[t]+1
+
+for i in range(0,1001):
+    li3[i]=li2[i]
+
+m1=max(li3)
+mi=li3.index(m1)
+print mi
