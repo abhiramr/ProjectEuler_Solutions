c08cf948caf9ff2125e8a49dc302fa1f9cf764d9
Abhiram <abhi.darkness@gmail.com>
2015-09-24 22:25:55 +0530

    Added words.txt for num42

--- num42.py
+++ num42.py
@@ -1,7 +1,7 @@
 from enum import Enum
 from string import maketrans
 alphabets=Enum('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
-f=open('/Users/aramesh/Desktop/wordsz.txt','r')
+f=open('./wordsz.txt','r')
 li=[]
 for l in f.readlines():
     li.append(l.strip().split(","))
